84f95331612662cffb605a4f61d8823c065264d0
Luciana Erneta <luciana.erneta@hotmail.com>
2021-10-14 14:45:53 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // selecciona los elementos aquí
+  
+    var boton = document.getElementById('envio');
+    boton.onclick = function () {
+      sendMail();
+    }
+  
+  
+  });
+  
+  function sendMail() {
+    var tempParams = {
+      from_name: document.getElementById('email').value,
+      to_name: "Mining Lists",
+      message: document.getElementById('message').value,
+      tel: document.getElementById('phoneNumber').value
+    };
+   
+    emailjs.send('service_nzcv2lf', 'template_co1yp9o', tempParams)
+      .then(function (res) {
+        alert("¡Mensaje enviado!");
+        document.getElementById('email').value="";
+        document.getElementById('message').value="";
+        document.getElementById('phoneNumber').value="";
+        document.getElementById('name').value="";
+      })
+  
+  }
